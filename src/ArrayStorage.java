437395a06aade0fc16c04220faeadd628ff1ff10
Elnur Shukurov <shukurovs.e@gmail.com>
2023-07-21 20:40:02 +0400

    Implement methods save, get, delete, clear, getAll, size in ArrayStorage.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,68 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume searchedResume = null;
+        for (Resume resume : storage) {
+            if (resume != null && resume.toString().equals(uuid)) {
+                searchedResume = resume;
+            }
+        }
+        return searchedResume;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].toString().equals(uuid)) {
+                storage[i] = null;
+                break;
+            }
+        }
+        for (int i = storage.length - 1; i >= 0; i--) {
+            if (storage[i] != null) {
+                for (int j = i; j > 0; j--) {
+                    if (storage[j] == null) {
+                        storage[j] = storage[j + 1];
+                    }
+                }
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] nonNullStorage = new Resume[size()];
+        int index = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                nonNullStorage[index] = resume;
+                index++;
+            }
+        }
+        return nonNullStorage;
     }
 
     int size() {
-        return 0;
+        int arraySize = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                arraySize++;
+            }
+        }
+        return arraySize;
     }
 }
