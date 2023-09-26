7287ac4737d06d25dfdb4daa669e36a460fc268c
ElnurShukurov <shukurovs.e@gmail.com>
2023-09-26 21:05:16 +0400

    Task 10 implementation

--- src/com/urise/webapp/storage/serialization/DataStreamSerialization.java
+++ src/com/urise/webapp/storage/serialization/DataStreamSerialization.java
@@ -5,8 +5,8 @@ import com.urise.webapp.model.*;
 import java.io.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 public class DataStreamSerialization implements StreamSerializationStrategy {
     @Override
@@ -15,48 +15,44 @@ public class DataStreamSerialization implements StreamSerializationStrategy {
             dos.writeUTF(r.getUuid());
             dos.writeUTF(r.getFullName());
 
-            Map<ContactType, String> contacts = r.getContacts();
-            dos.writeInt(contacts.size());
-            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
+            writeCollection(r.getContacts().entrySet(), dos, entry -> {
                 dos.writeUTF(entry.getKey().name());
                 dos.writeUTF(entry.getValue());
-            }
+            });
 
-            Map<SectionType, Section> sections = r.getSections();
-            dos.writeInt(sections.size());
-            for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
-                dos.writeUTF(entry.getKey().name());
+            writeCollection(r.getSections().entrySet(), dos, entry -> {
                 Section section = entry.getValue();
-                dos.writeUTF(section.getClass().getSimpleName());
+                SectionType sectionType = entry.getKey();
 
-                if (section instanceof TextSection) {
-                    dos.writeUTF(((TextSection) section).getContent());
-                } else if (section instanceof ListSection) {
-                    List<String> items = ((ListSection) section).getItems();
-                    dos.writeInt(items.size());
-                    for (String item : items) {
-                        dos.writeUTF(item);
-                    }
-                } else if (section instanceof CompanySection) {
-                    List<Company> companies = ((CompanySection) section).getCompanies();
-                    dos.writeInt(companies.size());
-                    for (Company company : companies) {
-                        dos.writeUTF(company.getHomepage().getName());
-                        dos.writeUTF(company.getHomepage().getUrl());
-                        dos.writeInt(company.getPeriods().size());
-                        for (Company.Period period : company.getPeriods()) {
-                            dos.writeUTF(period.getTitle());
-                            String description = period.getDescription();
-                            dos.writeBoolean(description != null);
-                            if (description != null) {
-                                dos.writeUTF(description);
-                            }
-                            dos.writeUTF(period.getStartDate().toString());
-                            dos.writeUTF(period.getEndDate().toString());
-                        }
-                    }
+                dos.writeUTF(sectionType.name());
+                switch (sectionType) {
+                    case PERSONAL:
+                    case OBJECTIVE:
+                        dos.writeUTF(((TextSection) section).getContent());
+                        break;
+                    case ACHIEVEMENT:
+                    case QUALIFICATIONS:
+                        writeCollection(((ListSection) section).getItems(), dos, dos::writeUTF);
+                        break;
+                    case EXPERIENCE:
+                    case EDUCATION:
+                        writeCollection(((CompanySection) section).getCompanies(), dos, company -> {
+                            dos.writeUTF(company.getHomepage().getName());
+                            dos.writeUTF(company.getHomepage().getUrl());
+                            writeCollection(company.getPeriods(), dos, period -> {
+                                dos.writeUTF(period.getTitle());
+                                String description = period.getDescription();
+                                dos.writeBoolean(description != null);
+                                if (description != null) {
+                                    dos.writeUTF(description);
+                                }
+                                dos.writeUTF(period.getStartDate().toString());
+                                dos.writeUTF(period.getEndDate().toString());
+                            });
+                        });
+                        break;
                 }
-            }
+            });
         }
     }
 
@@ -66,50 +62,70 @@ public class DataStreamSerialization implements StreamSerializationStrategy {
             String uuid = dis.readUTF();
             String fullName = dis.readUTF();
             Resume resume = new Resume(uuid, fullName);
-            int size = dis.readInt();
-            for (int i = 0; i < size; i++) {
-                resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
+
+            int contactsSize = dis.readInt();
+            for (int i = 0; i < contactsSize; i++) {
+                ContactType contactType = ContactType.valueOf(dis.readUTF());
+                String contactValue = dis.readUTF();
+                resume.addContact(contactType, contactValue);
             }
 
-            int numSections = dis.readInt();
-            for (int i = 0; i < numSections; i++) {
+            int sectionsSize = dis.readInt();
+            for (int i = 0; i < sectionsSize; i++) {
                 SectionType sectionType = SectionType.valueOf(dis.readUTF());
-                String sectionClassName = dis.readUTF();
-                Section section = null;
-
-                if (TextSection.class.getSimpleName().equals(sectionClassName)) {
-                    section = new TextSection(dis.readUTF());
-                } else if (ListSection.class.getSimpleName().equals(sectionClassName)) {
-                    int numItems = dis.readInt();
-                    List<String> items = new ArrayList<>();
-                    for (int j = 0; j < numItems; j++) {
-                        items.add(dis.readUTF());
-                    }
-                    section = new ListSection(items);
-                } else if (CompanySection.class.getSimpleName().equals(sectionClassName)) {
-                    int numCompanies = dis.readInt();
-                    List<Company> companies = new ArrayList<>();
-                    for (int j = 0; j < numCompanies; j++) {
-                        String companyName = dis.readUTF();
-                        String companyUrl = dis.readUTF();
-                        int numPeriods = dis.readInt();
-                        List<Company.Period> periods = new ArrayList<>();
-                        for (int k = 0; k < numPeriods; k++) {
-                            String periodTitle = dis.readUTF();
-                            boolean hasValue = dis.readBoolean();
-                            String periodDescription = hasValue ? dis.readUTF() : null;
-                            LocalDate startDate = LocalDate.parse(dis.readUTF());
-                            LocalDate endDate = LocalDate.parse(dis.readUTF());
-                            periods.add(new Company.Period(startDate, endDate, periodTitle, periodDescription));
+                switch (sectionType) {
+                    case PERSONAL:
+                    case OBJECTIVE:
+                        String text = dis.readUTF();
+                        resume.addSection(sectionType, new TextSection(text));
+                        break;
+                    case ACHIEVEMENT:
+                    case QUALIFICATIONS:
+                        int itemsSize = dis.readInt();
+                        List<String> items = new ArrayList<>();
+                        for (int j = 0; j < itemsSize; j++) {
+                            items.add(dis.readUTF());
                         }
-                        companies.add(new Company(new Link(companyName, companyUrl), periods));
-                    }
-                    section = new CompanySection(companies);
+                        resume.addSection(sectionType, new ListSection(items));
+                        break;
+                    case EXPERIENCE:
+                    case EDUCATION:
+                        int companiesSize = dis.readInt();
+                        List<Company> companies = new ArrayList<>();
+                        for (int j = 0; j < companiesSize; j++) {
+                            String companyName = dis.readUTF();
+                            String companyUrl = dis.readUTF();
+                            int periodsSize = dis.readInt();
+                            List<Company.Period> periods = new ArrayList<>();
+                            for (int k = 0; k < periodsSize; k++) {
+                                String periodTitle = dis.readUTF();
+                                boolean hasValue = dis.readBoolean();
+                                String periodDescription = hasValue ? dis.readUTF() : null;
+                                LocalDate startDate = LocalDate.parse(dis.readUTF());
+                                LocalDate endDate = LocalDate.parse(dis.readUTF());
+                                periods.add(new Company.Period(startDate, endDate, periodTitle, periodDescription));
+                            }
+                            companies.add(new Company(new Link(companyName, companyUrl), periods));
+                        }
+                        resume.addSection(sectionType, new CompanySection(companies));
+                        break;
                 }
-                resume.addSection(sectionType, section);
             }
+
             return resume;
         }
+    }
+
+    private <T> void writeCollection(Collection<T> collection, DataOutputStream dos, WriterAction<T> action) throws IOException {
+        dos.writeInt(collection.size());
+        for (T item : collection) {
+            action.write(item);
+        }
+    }
+
+    @FunctionalInterface
+    private interface WriterAction<T> {
+        void write(T t) throws IOException;
     }
 
 }
