e4d5d1157295f6e18ad57434b24dd601c2c6a9ad
ElnurShukurov <shukurovs.e@gmail.com>
2023-09-30 19:25:40 +0400

    Task 11, implement deadlock

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -6,7 +6,8 @@ import java.util.List;
 public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
     private int counter;
-    private static final Object LOCK = new Object();
+    private static final Object LOCK1 = new Object();
+    private static final Object LOCK2 = new Object();
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
@@ -14,8 +15,19 @@ public class MainConcurrency {
         Thread thread0 = new Thread() {
             @Override
             public void run() {
-                System.out.println(getName() + ", " + getState());
-                throw new IllegalStateException();
+                System.out.println(getName() + ", " + getState() + ": trying to get lock1");
+                synchronized (LOCK1) {
+                    System.out.println(getName() + " is holding lock1");
+                    System.out.println(getName() + ", " + getState() + ": trying to get lock2");
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    synchronized (LOCK2) {
+                        System.out.println(getName() + " is holding lock1 and lock2");
+                    }
+                }
             }
         };
         thread0.start();
@@ -24,7 +36,19 @@ public class MainConcurrency {
 
             @Override
             public void run() {
-                System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
+                System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState() + ": trying to get lock2");
+                synchronized (LOCK2) {
+                    System.out.println(Thread.currentThread().getName() + " is holding lock2");
+                    System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState() + ": trying to get lock1");
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    synchronized (LOCK1) {
+                        System.out.println(Thread.currentThread().getName() + " is holding lock1");
+                    }
+                }
             }
 
             private void inc() {
@@ -35,7 +59,7 @@ public class MainConcurrency {
 
         }).start();
 
-        System.out.println(thread0.getState());
+//        System.out.println(thread0.getState());
 
         final MainConcurrency mainConcurrency = new MainConcurrency();
         List<Thread> threadList = new ArrayList<>(THREADS_NUMBER);
@@ -59,6 +83,7 @@ public class MainConcurrency {
         });
         System.out.println(mainConcurrency.counter);
     }
+
 
     private synchronized void inc() {
         counter++;
