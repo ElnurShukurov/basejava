5c9cade858c5e970ffe8068c3fca0b79334f2e0e
ElnurShukurov <shukurovs.e@gmail.com>
2023-10-28 20:23:25 +0400

    Task 16, jsp and include

--- src/com/urise/webapp/web/ResumeServlet.java
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -1,7 +1,6 @@
 package com.urise.webapp.web;
 
 import com.urise.webapp.Config;
-import com.urise.webapp.model.Resume;
 import com.urise.webapp.storage.Storage;
 
 import javax.servlet.ServletConfig;
@@ -10,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
     private Storage storage;
@@ -26,34 +24,7 @@ public class ResumeServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-//        response.setHeader("Content-Type", "text/html; charset=UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-        Writer writer = response.getWriter();
-        writer.write(
-                "<html>\n" +
-                        "<head>\n" +
-                        "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
-                        "    <title>Resumes</title>\n" +
-                        "</head>\n" +
-                        "<body>\n" +
-                        "<section>\n" +
-                        "<table border=\"1\" cellpadding=\"10\">\n" +
-                        "    <tr>\n" +
-                        "        <th>Uuid</th>\n" +
-                        "        <th>Full-name</th>\n" +
-                        "    </tr>\n");
-        for (Resume resume : storage.getAllSorted()) {
-            writer.write(
-                    "<tr>\n" +
-                            "     <td>" + resume.getUuid() + "</td>\n" +
-                            "     <td>" + resume.getFullName() + "</td>\n" +
-                            "</tr>\n");
-        }
-        writer.write("</table>\n" +
-                "</section>\n" +
-                "</body>\n" +
-                "</html>\n");
+        request.setAttribute("resumes", storage.getAllSorted());
+        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
     }
 }
