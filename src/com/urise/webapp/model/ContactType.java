0996b9f332dd962ca178fceb064a608c5b65ac2f
ElnurShukurov <shukurovs.e@gmail.com>
2023-11-02 22:52:52 +0400

    Task 16, complete crud commit

--- src/com/urise/webapp/model/ContactType.java
+++ src/com/urise/webapp/model/ContactType.java
@@ -5,21 +5,41 @@ public enum ContactType {
     SKYPE("Skype") {
         @Override
         public String toHtml0(String value) {
-            return "<a href='skype: " + value + "'>" + value + "</a>";
+            return getTitle() + ": " + toLink("skype: " + value, value);
         }
     },
     EMAIL("Почта") {
         @Override
         public String toHtml0(String value) {
-            return "<a href='mailto: " + value + "'>" + value + "</a>";
+            return getTitle() + ": " + toLink("mailto: " + value, value);
         }
     },
-    LINKEDIN("Профиль LinkedIn"),
-    GITHUB("Профиль GitHub"),
-    STACKOVERFLOW("Профиль StackOverFlow"),
-    HOME_PAGE("Домашняя страница");
+    LINKEDIN("Профиль LinkedIn") {
+        @Override
+        public String toHtml0(String value) {
+            return toLink(value);
+        }
+    },
+    GITHUB("Профиль GitHub") {
+        @Override
+        public String toHtml0(String value) {
+            return toLink(value);
+        }
+    },
+    STACKOVERFLOW("Профиль StackOverFlow") {
+        @Override
+        public String toHtml0(String value) {
+            return toLink(value);
+        }
+    },
+    HOME_PAGE("Домашняя страница") {
+        @Override
+        public String toHtml0(String value) {
+            return toLink(value);
+        }
+    };
 
-    private String title;
+    private final String title;
 
     ContactType(String title) {
         this.title = title;
@@ -35,5 +55,13 @@ public enum ContactType {
 
     public String toHtml(String value) {
         return (value == null) ? "" : toHtml0(value);
+    }
+
+    public String toLink(String href) {
+        return toLink(href, title);
+    }
+
+    public static String toLink(String href, String title) {
+        return "<a href='" + href + "'>" + title + "</a>";
     }
 }
